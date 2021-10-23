fb106d4dfabb66b6cb872257ac8143e87140c49d
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-10-23 20:23:27 +0100

    feat: Added zuul Filters

--- gateway-server/src/main/java/com/esprit/gateway/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/esprit/gateway/GatewayServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import com.esprit.gateway.filters.*;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +14,26 @@ public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+	
+	/*@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}*/
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 
 }
