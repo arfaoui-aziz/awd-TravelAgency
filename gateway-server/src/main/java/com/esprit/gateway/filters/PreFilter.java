fb379c0618304ff29de4f18529642c7473fc1cf3
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-10-23 18:08:40 +0100

    feat: zuul api gateway added

---
+++ gateway-server/src/main/java/com/esprit/gateway/filters/PreFilter.java
@@ -0,0 +1,42 @@
+package com.esprit.gateway.filters;
+
+import org.springframework.stereotype.Component;
+import javax.servlet.http.HttpServletRequest;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class PreFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+	
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+
+		System.out.println(
+				"Using Pre filter    Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+
+		return 0;
+	}
+
+}
